cf56617898bcf9efba8758ba8f155134d51cc3f5
TITANSOFT\roy.xu23 <roy.xu@titansoft.com.sg>
2024-03-27 16:24:41 +0800

    update test

--- ContactManagement/Models/Contact.cs
+++ ContactManagement/Models/Contact.cs
@@ -8,11 +8,16 @@ namespace ContactManagement.Models
     {
         [Key]
         public int Id { get; set; }
+        
         [Required]
+        [StringLength(16, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 16 characters.")]
         public string Name { get; set; }
-        [DisplayName("Number")]
+        
         [Required]
+        [StringLength(12, MinimumLength = 5, ErrorMessage = "Number must be between 5 and 12 digits.")]
+        [RegularExpression("^[0-9]+$", ErrorMessage = "Number must be digits only.")]
         public string Number { get; set; }
+        
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
